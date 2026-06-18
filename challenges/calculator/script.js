a85a16f272c59f850f8059013f8567a861091a6e
Bishwajeetdev <bishwajeetsingh355@email.com>
2026-06-18 14:40:01 +0530

    calculator application build through js

--- challenges/calculator/script.js
+++ challenges/calculator/script.js
@@ -1,4 +1,49 @@
-const inputField = document.getElementById("inputfield");
-const buttons =document.getElementById("clear")
-buttons.innerText
-console.log(buttons)
+const display = document.getElementById("inputfield");
+const buttons = document.querySelectorAll(".buttons");
+
+display.classList.add("inputDisplay");
+
+let resultShown =false;
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const value = button.textContent;
+
+    if (value === "c") {
+      display.textContent = "";
+      resultShown=false;
+    } else if (value === "=") {
+      if (display.textContent == "") {
+        display.textContent = "Enter a number";
+      } else {
+        try {
+          const result = eval(display.textContent);
+
+          if (result == Infinity) {
+            display.textContent = "Cannot Divided by Zero";
+          } else {
+            display.textContent = result;
+            resultShown=true;
+          }
+        } catch (error) {
+          display.textContent = "Invalid Input";
+        }
+      }
+    } else {
+        if(resultShown){
+            const operator=['+','-','/','*',];
+            if(operator.includes(value)){
+                display.textContent += value;
+
+            }else{
+                   display.textContent = value;
+
+            }
+            resultShown= false;
+        }else{
+                display.textContent += value;
+
+        }
+    }
+  });
+});
